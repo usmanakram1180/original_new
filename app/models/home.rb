1e1bc7aec8e41bebd879d507063fb3b6be7d33de
usmanakram1180 <usmanakram1180@outlook.com>
2024-03-22 16:35:48 +0500

    first commit

---
+++ app/models/home.rb
@@ -0,0 +1,10 @@
+class Home < ApplicationRecord
+
+  has_many :comments, dependent: :destroy
+  belongs_to :user
+    has_many_attached :music
+    has_one_attached :image
+    has_many_attached :images
+  has_one_attached :video
+  has_one_attached :photo
+end
